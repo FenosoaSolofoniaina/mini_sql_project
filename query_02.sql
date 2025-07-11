74ce17725a14851576c230fb44ec5f15f03ca608
FenosoaSolofoniaina <solofoniainafenosoa020@gmail.com>
2025-07-11 21:21:08 +0300

    create file : query__02.sql

---
+++ query_02.sql
@@ -0,0 +1,18 @@
+-- INSERTING RECORDS
+INSERT INTO
+    Customers
+VALUES
+    (6 'Anna', 'USA', NULL),
+    (7, 'Sam', 'UK', 100);
+
+SELECT *
+FROM Customers;
+
+-- INSERTING RECORDS
+INSERT INTO
+    Customers
+VALUES
+    (9, 'Andreas', 'Germany', NULL)
+
+SELECT *
+FROM Customers;
