f719df5d299d708c234639776c11fa8815bde340
FenosoaSolofoniaina <solofoniainafenosoa020@gmail.com>
2025-07-11 21:55:12 +0300

    update file : add text into 'query_02.sql'file

--- query_02.sql
+++ query_02.sql
@@ -8,6 +8,7 @@ VALUES
 SELECT *
 FROM Customers;
 
+
 -- INSERTING RECORDS
 INSERT INTO
     Customers
@@ -16,3 +17,40 @@ VALUES
 
 SELECT *
 FROM Customers;
+
+
+-- INSERTING RECORDS FROM ANOTHER TABLE
+INSERT INTO
+    Persons
+SELECT 
+    "id",
+    "name",
+    NULL,
+    'Unknown'
+FROM Customers;
+
+SELECT *
+FROM Persons;
+
+
+-- UPDATING DATA
+SELECT *
+FROM Customers;
+
+UPDATE Customers
+SET "id"=8
+WHERE "id"=9;
+
+UPDATE Customers
+SET "score" = 0
+WHERE "score" IS NULL;
+
+SELECT *
+FROM Customers;
+
+
+-- DELETING DATA
+DELETE FROM Persons
+WHERE "id" > 5;
+
+TRUNCATE TABLE Persons;
