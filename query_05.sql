f92ea7845c8d795ea73ca527d9afea5757b3b8fb
FenosoaSolofoniaina <solofoniainafenosoa020@gmail.com>
2025-07-14 20:59:28 +0300

    update file : add text into 'query_05.sql' file'

---
+++ query_05.sql
@@ -0,0 +1,20 @@
+SELECT
+    "id",
+    "level_of_score",
+    UPPER("level_of_score") AS up_level,
+    NULLIF(TRIM("level_of_score"), '') AS process_1
+FROM 
+(
+    SELECT
+        "id",
+        "first_name",
+        "last_name",
+        "score",
+        CASE
+            WHEN "score" > 600 THEN 'High'
+            WHEN "score" > 300 THEN 'Medium'
+            WHEN "score" > 0 THEN 'Low'
+            ELSE ''
+        END AS "level_of_score"
+    FROM Customers
+);
