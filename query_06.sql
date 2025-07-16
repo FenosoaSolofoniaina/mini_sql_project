bba16e0cb758419e7c6cb0c3204fdec69170511a
FenosoaSolofoniaina <solofoniainafenosoa020@gmail.com>
2025-07-16 20:16:05 +0300

    create file : 'query_06.sql'

---
+++ query_06.sql
@@ -0,0 +1,37 @@
+-- REMOVE TABLE
+DROP TABLE IF EXISTS Customers ;
+
+-- CREATE NEW TABLE
+CREATE TABLE IF NOT EXISTS Customers(
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(50),
+    "country" VARCHAR(50) NOT NULL,
+    "score" FLOAT
+);
+
+
+-- INSERT DATA
+INSERT INTO
+    Customers("name", "country", "score")
+VALUES
+    ('Maria', 'Germany', 350),
+    ('George', 'UK', 750),
+    ('Martin', 'Germany', 500),
+    ('John', 'USA', 900),
+    ('Peter', 'USA', 0);
+
+
+-- LOOK UP INSIDE THE TABLE
+SELECT *
+FROM Customers
+ORDER BY "score" DESC;
+
+
+-- WINDOW FUNCTION WITH SUM()
+SELECT
+	"id",
+    "name",
+    "country",
+    "score",
+    SUM("score") OVER (PARTITION BY "country" ORDER BY "name" ROWS UNBOUNDED PRECEDING AND CURRENT) score_per_country
+FROM Customers;
