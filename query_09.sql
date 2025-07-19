d6f27e6e7fa4d77854fb3f6daef0929933493fa4
FenosoaSolofoniaina <solofoniainafenosoa020@gmail.com>
2025-07-19 18:56:35 +0300

    update file : add some text into 'query_09.sql' file

--- query_09.sql
+++ query_09.sql
@@ -1,2 +1,24 @@
+/*
+ * SUBQUERY
+*/
+
+-- METADATA
 SELECT *
-FROM INFORMATION_SCHEMA;
+FROM INFORMATION_SCHEMA.COLUMNS;
+
+
+-- SUBQUERY
+-- This is the main query
+SELECT 
+	*,
+    CASE WHEN "score" > Average_score THEN 1
+         WHEN "score" < Average_score THEN -1
+         WHEN "score" = Average_score THEN 0
+    END Upper_than_average
+ FROM (
+    -- This is a subquery
+    SELECT
+        *,
+        AVG("score") OVER() Average_score
+    FROm Customers
+);
